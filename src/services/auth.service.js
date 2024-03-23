4524b0692609d72e87beacb34efb26f640879253
iamchrisas <chrisas.agb@gmail.com>
2024-03-23 17:51:23 +0100

    change base url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class AuthService {
   constructor() {
     this.api = axios.create({
-      baseURL: import.meta.env.SERVER_URL || "http://localhost:5005",
+      baseURL: import.meta.env.VITE_APP_BACKEND_URL || "http://localhost:5005",
     });
 
     // Middleware
