901c4f642a426b7861a9b56d9dc689836077ef4d
iamchrisas <chrisas.agb@gmail.com>
2024-03-25 23:50:18 +0100

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,28 +2,27 @@ import React, { useState, useContext } from "react";
 import authService from "../services/auth.service";
 import { AuthContext } from "../context/auth.context";
 import { Link, useNavigate } from "react-router-dom";
-
 function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const [error, setError] = useState("");
+  const [error, setError] = useState(""); // State to hold login error
   const navigate = useNavigate();
   const { storeToken, authenticateUser } = useContext(AuthContext);
-
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await authService.login(formData);
-      storeToken(response.data.authToken);
-      authenticateUser();
-      navigate("/user-profile");
-    } catch (err) {
-      console.error("Login Error:", err);
-      setError("Failed to login. Please check your email and password.");
-    }
+    authService
+      .login(formData)
+      .then((response) => {
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/user-profile");
+      })
+      .catch((err) => {
+        console.error("Login Error:", err);
+        setError("Failed to login. Please check your email and password."); // Provide user feedback
+      });
   };
 
   return (
@@ -46,10 +45,10 @@ function Login() {
         />
         <button type="submit">Login</button>
       </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <p>Not a user? <Link to="/signup">Sign Up</Link></p>
+      <p>
+        Not a user - <Link to={"/signup"}>SignUp</Link>
+      </p>
     </div>
   );
 }
-
 export default Login;
