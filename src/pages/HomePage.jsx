d8750613fac2698ed3808e548e08425090673032
iamchrisas <chrisas.agb@gmail.com>
2024-04-01 20:23:56 +0200

    add short video

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,21 +4,52 @@ function HomePage() {
   return (
     <div className="home-page glass-effect bg-gray-100">
       <header className="text-center py-6">
-        <h1 className="text-4xl font-bold">Macrotracker</h1>
+        <h1 className="text-4xl font-bold mb-2">Macrotracker</h1>
+        <h2>
+          <i>Get your summer body one meal at a time</i>
+        </h2>
       </header>
-      <div className="flex flex-col justify-center items-center">
-        <div className="max-w-4xl mx-auto p-4">
+      <div className="flex flex-col justify-center items-center mt-0">
+        <div className="max-w-4xl mx-auto">
           <main>
-            <section className="text-lg space-y-4">
-              <ul className="list-disc space-y-2 pl-5">
+            <section className="text-lg space-y-2">
+              <ul className="list-disc space-y-1 pl-5">
                 <li>Want to lose weight or gain muscle?</li>
-                <li>You're not alone.</li>
-                <li>Sticking to a diet often brings anxiety and guilt.</li>
+                <li>You don't have to feel anxiety or guilt.</li>
                 <li>
                   Close the gap between your goals and your habits, stress-free.
                 </li>
               </ul>
-              <p style={{ display:"flex" ,marginTop: "80px", justifyContent: "center" }}>
+              <div
+                style={{
+                  position: "relative",
+                  paddingBottom: "56.25%",
+                  marginTop: "30px",
+                  height: "0",
+                }}
+              >
+                <iframe
+                  style={{
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "100%",
+                    border: "0",
+                  }}
+                  src="https://www.tella.tv/video/cluh8dwlb012m0fidfq1qhz9z/embed?b=0&title=0&a=0&loop=0&autoPlay=true&t=0&muted=1&wt=0"
+                  allowFullScreen
+                  allowTransparency
+                ></iframe>
+              </div>
+              <p
+                style={{
+                  display: "flex",
+                  marginTop: "15px",
+                  padding: "20px",
+                  justifyContent: "center",
+                }}
+              >
                 Made by Chrisas
               </p>
             </section>
